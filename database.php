e7aa57e84d1cb38e5be01b943abcdf0e32349919
See Toh Yu Xiang <129309054+JSTYX04@users.noreply.github.com>
2024-03-27 16:48:34 +0800

    shabi see toh

--- database.php
+++ database.php
@@ -7,3 +7,5 @@ $connect= mysqli_connect("localhost","root","","JBP");// fill out database name
 <?php
 $result = mysqli_connect("Select * from staff where id <1000")
 ?>
+
+see toh is freerider and qi guan yan
