f845a5d808ce8f3855fc7fa965120fd8cf8b4a39
1211109437 <1211109437@student.mmu.edu.my>
2024-05-02 23:52:25 +0800

    a

--- shop.php
+++ shop.php
@@ -24,7 +24,7 @@
         // Database connection
         include('database.php');
         // Fetch featured products from the database
-        $query = "SELECT Product_name, Product_details, Product_price, Product_image FROM Products WHERE Product_ID IN (1, 11, 21, 31, 41)";
+        $query = "SELECT Product_name, Product_details, Product_price FROM Products WHERE Product_ID IN (1, 11, 21, 31, 41)";
         $result = mysqli_query($connect, $query);
 
         // Display featured products
