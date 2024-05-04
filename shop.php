7e1b9ea7a097186614e51e123cb6bf1a98d866f7
1211109437 <1211109437@student.mmu.edu.my>
2024-05-04 22:05:22 +0800

    a

--- shop.php
+++ shop.php
@@ -14,9 +14,12 @@
     </header>
     
     <nav>
-    <a href="index.html" class="button">⬅ Back To Homepage     <br></a> |
-    <a href="about.html" class="button">About Us     <br></a> |
-    <a href="about.html" class="button">Contact Us </a>
+    <a href="index.html" class="button">⬅ Back To Homepage</a>
+    <span class="spacer"></span>
+    <a href="about.html" class="button">About Us</a>
+    <span class="spacer"></span>
+    <a href="about.html" class="button">Contact Us</a>
+
 
     </nav>
     <section id="featured-products">
