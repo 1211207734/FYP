6616cc03487212890eff7ec11271fdfdaeffc8b0
JSTYX04 <1211207620@mmu.edu.my>
2024-05-09 14:13:21 +0800

    hello

--- cart.php
+++ cart.php
@@ -7,131 +7,138 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"">
 	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 	<link rel="stylesheet" href="css/cart.css">
-	<link rel="stylesheet" href="css/account.css">
 
 </head>
+<?php
 
-<body>
-	<main class="page">
-	 	<section class="shopping-cart dark">
-	 		<div class="container">
-		        <div class="block-heading">
-		          <h2>Shopping Cart</h2>
-		          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
-		        </div>
-		        <div class="content">
-	 				<div class="row">
-	 					<div class="col-md-12 col-lg-8">
-	 						<div class="items">
-				 				<div class="product">
-				 					<div class="row">
-					 					<div class="col-md-3">
-					 						<img class="img-fluid mx-auto d-block image" src="assets/img/image.jpg">
-					 					</div>
-					 					<div class="col-md-8">
-					 						<div class="info">
-						 						<div class="row">
-							 						<div class="col-md-5 product-name">
-							 							<div class="product-name">
-								 							<a href="#">Lorem Ipsum dolor</a>
-								 							<div class="product-info">
-									 							<div>Display: <span class="value">5 inch</span></div>
-									 							<div>RAM: <span class="value">4GB</span></div>
-									 							<div>Memory: <span class="value">32GB</span></div>
-									 						</div>
-									 					</div>
-							 						</div>
-							 						<div class="col-md-4 quantity">
-							 							<label for="quantity">Quantity:</label>
-							 							<input id="quantity" type="number" value ="1" class="form-control quantity-input">
-							 						</div>
-							 						<div class="col-md-3 price">
-							 							<span>$120</span>
-							 						</div>
-							 					</div>
-							 				</div>
-					 					</div>
-					 				</div>
-				 				</div>
-				 				<div class="product">
-				 					<div class="row">
-					 					<div class="col-md-3">
-					 						<img class="img-fluid mx-auto d-block image" src="assets/img/image.jpg">
-					 					</div>
-					 					<div class="col-md-8">
-					 						<div class="info">
-						 						<div class="row">
-							 						<div class="col-md-5 product-name">
-							 							<div class="product-name">
-								 							<a href="#">Lorem Ipsum dolor</a>
-								 							<div class="product-info">
-									 							<div>Display: <span class="value">5 inch</span></div>
-									 							<div>RAM: <span class="value">4GB</span></div>
-									 							<div>Memory: <span class="value">32GB</span></div>
-									 						</div>
-									 					</div>
-							 						</div>
-							 						<div class="col-md-4 quantity">
-							 							<label for="quantity">Quantity:</label>
-							 							<input id="quantity" type="number" value ="1" class="form-control quantity-input">
-							 						</div>
-							 						<div class="col-md-3 price">
-							 							<span>$120</span>
-							 						</div>
-							 					</div>
-							 				</div>
-					 					</div>
-					 				</div>
-				 				</div>
-				 				<div class="product">
-				 					<div class="row">
-					 					<div class="col-md-3">
-					 						<img class="img-fluid mx-auto d-block image" src="assets/img/image.jpg">
-					 					</div>
-					 					<div class="col-md-8">
-					 						<div class="info">
-						 						<div class="row">
-							 						<div class="col-md-5 product-name">
-							 							<div class="product-name">
-								 							<a href="#">Lorem Ipsum dolor</a>
-								 							<div class="product-info">
-									 							<div>Display: <span class="value">5 inch</span></div>
-									 							<div>RAM: <span class="value">4GB</span></div>
-									 							<div>Memory: <span class="value">32GB</span></div>
-									 						</div>
-									 					</div>
-							 						</div>
-							 						<div class="col-md-4 quantity">
-							 							<label for="quantity">Quantity:</label>
-							 							<input id="quantity" type="number" value ="1" class="form-control quantity-input">
-							 						</div>
-							 						<div class="col-md-3 price">
-							 							<span>$120</span>
-							 						</div>
-							 					</div>
-							 				</div>
-					 					</div>
-					 				</div>
-				 				</div>
-				 			</div>
-			 			</div>
-			 			<div class="col-md-12 col-lg-4">
-			 				<div class="summary">
-			 					<h3>Summary</h3>
-			 					<div class="summary-item"><span class="text">Subtotal</span><span class="price">$360</span></div>
-			 					<div class="summary-item"><span class="text">Discount</span><span class="price">$0</span></div>
-			 					<div class="summary-item"><span class="text">Shipping</span><span class="price">$0</span></div>
-			 					<div class="summary-item"><span class="text">Total</span><span class="price">$360</span></div>
-								<a href="payment.php?eml=<?php echo $emml ?>"><button type="button"class="rbut" >Add Card</button></a>
-								<button type="button" class="btn btn-primary btn-lg btn-block">Checkout</button>
-				 			</div>
-			 			</div>
-		 			</div> 
-		 		</div>
-	 		</div>
-		</section>
-	</main>
-</body>
+	$connect= mysqli_connect("localhost","root","","jbp");
+	if (isset($_GET['eml'])) {
+		$emml = $_GET['eml'];}
+		$result = mysqli_query($connect, "SELECT Customer_password FROM customer where Customer_ID='$emml'");
+		while($row = mysqli_fetch_assoc($result)) 
+?>
+	<body>
+		<main class="page">
+			<section class="shopping-cart dark">
+				<div class="container">
+					<div class="block-heading">
+					<h2>Shopping Cart</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p>
+					</div>
+					<div class="content">
+						<div class="row">
+							<div class="col-md-12 col-lg-8">
+								<div class="items">
+									<div class="product">
+										<div class="row">
+											<div class="col-md-3">
+												<img class="img-fluid mx-auto d-block image" src="assets/img/image.jpg">
+											</div>
+											<div class="col-md-8">
+												<div class="info">
+													<div class="row">
+														<div class="col-md-5 product-name">
+															<div class="product-name">
+																<a href="#">Lorem Ipsum dolor</a>
+																<div class="product-info">
+																	<div>Display: <span class="value">5 inch</span></div>
+																	<div>RAM: <span class="value">4GB</span></div>
+																	<div>Memory: <span class="value">32GB</span></div>
+																</div>
+															</div>
+														</div>
+														<div class="col-md-4 quantity">
+															<label for="quantity">Quantity:</label>
+															<input id="quantity" type="number" value ="1" class="form-control quantity-input">
+														</div>
+														<div class="col-md-3 price">
+															<span>$120</span>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="product">
+										<div class="row">
+											<div class="col-md-3">
+												<img class="img-fluid mx-auto d-block image" src="assets/img/image.jpg">
+											</div>
+											<div class="col-md-8">
+												<div class="info">
+													<div class="row">
+														<div class="col-md-5 product-name">
+															<div class="product-name">
+																<a href="#">Lorem Ipsum dolor</a>
+																<div class="product-info">
+																	<div>Display: <span class="value">5 inch</span></div>
+																	<div>RAM: <span class="value">4GB</span></div>
+																	<div>Memory: <span class="value">32GB</span></div>
+																</div>
+															</div>
+														</div>
+														<div class="col-md-4 quantity">
+															<label for="quantity">Quantity:</label>
+															<input id="quantity" type="number" value ="1" class="form-control quantity-input">
+														</div>
+														<div class="col-md-3 price">
+															<span>$120</span>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="product">
+										<div class="row">
+											<div class="col-md-3">
+												<img class="img-fluid mx-auto d-block image" src="assets/img/image.jpg">
+											</div>
+											<div class="col-md-8">
+												<div class="info">
+													<div class="row">
+														<div class="col-md-5 product-name">
+															<div class="product-name">
+																<a href="#">Lorem Ipsum dolor</a>
+																<div class="product-info">
+																	<div>Display: <span class="value">5 inch</span></div>
+																	<div>RAM: <span class="value">4GB</span></div>
+																	<div>Memory: <span class="value">32GB</span></div>
+																</div>
+															</div>
+														</div>
+														<div class="col-md-4 quantity">
+															<label for="quantity">Quantity:</label>
+															<input id="quantity" type="number" value ="1" class="form-control quantity-input">
+														</div>
+														<div class="col-md-3 price">
+															<span>$120</span>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-12 col-lg-4">
+								<div class="summary">
+									<h3>Summary</h3>
+									<div class="summary-item"><span class="text">Subtotal</span><span class="price">$360</span></div>
+									<div class="summary-item"><span class="text">Discount</span><span class="price">$0</span></div>
+									<div class="summary-item"><span class="text">Shipping</span><span class="price">$0</span></div>
+									<div class="summary-item"><span class="text">Total</span><span class="price">$360</span></div>
+									<a href="payment.php?eml=<?php echo $emml ?>"><button type="button"class="rbut" >Add Card</button></a>
+									<button type="button" class="btn btn-primary btn-lg btn-block">Checkout</button>
+								</div>
+							</div>
+						</div> 
+					</div>
+				</div>
+			</section>
+		</main>
+	</body>
+
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
