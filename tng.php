38cfe577f32d0e01664d0612c2dcdedd1bf90912
JSTYX04 <1211207620@mmu.edu.my>
2024-05-28 23:35:56 +0800

    heiyo

--- tng.php
+++ tng.php
@@ -9,7 +9,7 @@
 include('database.php');
 if (isset($_GET['eml'])) {
     $emml = $_GET['eml'];}
-	if (isset($_GET['tt'])) {
+if (isset($_GET['tt'])) {
 		$total = $_GET['tt'];}
 	$connect= mysqli_connect("localhost","root","","jbp");
 	$result = mysqli_query($connect, "SELECT * FROM tng where Customer_ID='$emml'");
@@ -23,15 +23,50 @@ if (isset($_GET['eml'])) {
 <body>
 	
 	<div>Total Amount Needed to Pay : <?php echo $total ?> </div>
-	<div>Balance of TNG Wallet : <a><?php echo $row['Balance']?></a></div>
-	
-	<div>
-	<a href="cart.php?eml=<?php echo $emml ?>">Back to Cart</a>
-	<a href="checkout.php?eml=<?php echo $emml ?>">Proceed Payment</a>
-	</div>
-	<div>
-	<button class="button">Reload RM50</button><button class="button">Reload RM100</button>
-	</div>
+	<div>Balance of TNG Wallet : <a><?php echo $b=$row['Balance']?></a></div>
+	<form method="post">
+		<div>
+			<a href="cart.php?eml=<?php echo $emml ?>">Back to Cart</a>
+			<button type="submit" name="pay">Proceed Payment</button>
+		</div>
+		<div>
+			<button class="button" type="submit" name="r50">Reload RM50</button>
+			<button class="button" type="submit" name="r100">Reload RM100</button>
+		</div>
+		<?php
+		if (isset($_POST['r50'])) {
+			$b += 50;
+			$update = mysqli_query($connect, "UPDATE tng SET Balance='$b' WHERE Customer_ID='$emml'");
+			echo '<script>';
+			echo 'alert("Reload RM50 Successfully!") ;';
+			echo 'window.location.href = "tng.php?eml=' . $emml . '&tt='.$total.'";';	 
+			echo '</script>';
+			exit();
+		}
+		else if (isset($_POST['r100'])) {
+			$b += 100;
+			$update = mysqli_query($connect, "UPDATE tng SET Balance='$b' WHERE Customer_ID='$emml'");
+			echo '<script>';
+			echo 'alert("Reload RM100 Successfully!") ;';
+			echo 'window.location.href = "tng.php?eml=' . $emml . '&tt='.$total.'";';	 
+			echo '</script>';	
+			exit();	
+		}?>
+	</form>
 	
 </body>
 <?php }
+if (isset($_POST['pay'])) {
+	if ($b >= $total){
+	 echo '<script>';
+	 echo 'window.location.href = "checkout.php?eml='.$emml.'";';
+	 echo '</script>';
+	}
+
+ 	else {
+
+	 echo '<script>';
+	 echo 'alert("Low Balance ! Please Reload !") ;';	 
+	 echo '</script>';  }                      
+} 
+?>
